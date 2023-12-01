c90881724871554b3ff45884acfe8bcfd5f8b6b3
DESKTOP-AAHU0IQ\Tilo <info@tilorenner.com>
2023-12-01 16:58:08 +0100

    unifed json for pokedata

--- utils/pokeDataCombiner.js
+++ utils/pokeDataCombiner.js
@@ -18,20 +18,48 @@ const PokeDataCombiner = {
             const pokemon = pokedata_source[i];
             const name = pokemon.name.english.toLowerCase()
             console.log(name)
-            //onsole.log("Poke " ,pokedata_source[i])
-            //const types = pokemon.type
-            //for(type in pokemon.type)
-            //{
-
-            //}
             try{
-                
-                //const response = await fetch(`https://pokeapi.co/api/v2/type/${pokemon.name.english}/`)
-                //console.log(response)
-                //const resultTypes = await response.json();
+                let damages = {
+                    double_damage_from: [],
+                    half_damage_from:[],
+                    no_damage_from:[],
+                    double_damage_to: [],
+                    half_damage_to:[],
+                    no_damage_to:[],
+                }
+                for(let y = 0; y < pokemon.type.length; y++)
+                {
+                    const poketype = pokemon.type[y];
+                    console.log("TYPE:" , poketype)
+                    const response = await fetch(`https://pokeapi.co/api/v2/type/${poketype.toLowerCase()}/`)
+                    //console.log(response)
+                    const resultPokeType = await response.json();
+                    console.log("TypeData:", resultPokeType.damage_relations)
+                    resultPokeType.damage_relations.double_damage_from.forEach(element => {
+                        damages.double_damage_from.push(element)
+                    });
+                    resultPokeType.damage_relations.half_damage_from.forEach(element => {
+                        damages.half_damage_from.push(element)
+                    });
+                    resultPokeType.damage_relations.no_damage_from.forEach(element => {
+                        damages.no_damage_from.push(element)
+                    });
+                    resultPokeType.damage_relations.double_damage_to.forEach(element => {
+                        damages.double_damage_to.push(element)
+                    });
+                    resultPokeType.damage_relations.half_damage_to.forEach(element => {
+                        damages.half_damage_to.push(element)
+                    });
+                    resultPokeType.damage_relations.no_damage_to.forEach(element => {
+                        damages.no_damage_to.push(element)
+                    });
+
+                }
+                pokemon.damage_relations = damages
+
 
                 let fetchString = `https://pokeapi.co/api/v2/pokemon/${name}`;
-                console.log("String:" ,fetchString);
+                //npm console.log("String:" ,fetchString);
                 const response2 = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
                 const resultPokemon = await response2.json();
                 //pokemon.damage_relations = resultTypes.damage_relations;
