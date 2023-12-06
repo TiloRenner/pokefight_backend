1ae9f7b0eab4f8078f3652d7c0aa77bd28e0ffe3
DESKTOP-AAHU0IQ\Tilo <info@tilorenner.com>
2023-12-06 14:05:32 +0100

    login also works as register route

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import cors from 'cors'
-import PokeDataCombiner from './utils/pokeDataCombiner.js'
+//import PokeDataCombiner from './utils/pokeDataCombiner.js'
 //import IndexRouter from './router/indexRouter.js'
 import PokemonRouter from './router/pokemonRouter.js'
 import LoginRouter from './router/loginRouter.js'
 import RegisterRouter from './router/registerRouter.js'
+import cookieParser from 'cookie-parser'
 import 'dotenv/config'
 
 const {PORT} = process.env
@@ -12,9 +13,19 @@ console.log(PORT)
 
 const app = express()
 
-app.use(cors())
+
+const allowCrossDomain = (req, res, next) => {
+    //res.header(`Access-Control-Allow-Origin`, `http://localhost/`);
+    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+    next();
+  };
+
+app.use(cors({credentials: true, origin: `http://localhost:5173`, methods:[`GET,PUT,POST,DELETE`],allowedHeaders:[`Content-Type`,`Authorization`]}))
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+//app.use(allowCrossDomain)
 
 //app.use("/", IndexRouter)
 app.use("/pokemon", PokemonRouter)
---
+++ middlewares/jwtCookieVerify.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
+
+
+const jwtCookieVerify = function (req,res,next)
+{
+    console.log("Cookies:" , req.cookies)
+    const token = req.cookies.pokefight_token;
+    if(token)
+    {
+        try{
+            const user = jwt.verify(token,process.env.TOKEN_SECRET);
+            req.user = user
+            console.log("identified user", user)
+            next()
+        }
+        catch(err)
+        {
+            console.log("ErrorAuth:", err.message)
+            res.clearCookie("pokefight_token");
+            res.send({msg:"access denied, please login"})
+        }
+    }
+    else{
+        res.send({msg:"access denied, please login"})
+    }
+}
+
+function rejectUser(err,res)
+{
+
+
+}
+
+export default jwtCookieVerify;
