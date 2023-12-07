6f6c158f015a430277d0324003b6bbe2e0f6f349
DESKTOP-AAHU0IQ\Tilo <info@tilorenner.com>
2023-12-07 16:14:57 +0100

    add /user route

---
+++ router/userRouter.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import jwtCookieVerify from '../middlewares/jwtCookieVerify.js'
+import MongooseUser from '../utils/mongooseUser.js';
+
+
+const router = express.Router()
+
+router.get("/",jwtCookieVerify,  async (req,res)=> {
+
+    const username = req.user.username;
+    console.log("User:", req.user)
+    const userExists = await MongooseUser.findUser(username)
+    userExists[0].password =""
+    console.log("userResult: ", userExists)
+    res.json(userExists[0])
+
+
+})
+
+export default router
